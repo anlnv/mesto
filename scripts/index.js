95ba5c58ae7912cfa268b42b54acf0d07a933efe
Anna Luneva <aaluneva@edu.hse.ru>
2023-03-02 18:14:55 +0300

    5 project work first commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,159 @@
-const popupElement = document.querySelector('.popup');
-const popupFormElement = document.querySelector('.popup__form');
-const popupCloseButtonElement = document.querySelector('.popup__button-close');
-const popupOpenButtonElement = document.querySelector('.profile__edit-button');
+const profilePopup = document.querySelector('#profile-popup');
+const profileForm = profilePopup.querySelector('#profile-form');
+const buttonClose = document.querySelectorAll('.popup__button-close');
+const profileEdit = document.querySelector('.profile__edit-button');
 
-//profile edition
+
+const createPopup = document.querySelector('#create-popup');
+const buttonAdd = document.querySelector('.profile__add-button');
+const createForm = document.querySelector('#create-form');
+const createFormLink = document.querySelector('#link');
+const createFormTitle = document.querySelector('#title');
+
 
 const profileElement = document.querySelector('.profile');
 const profileName = profileElement.querySelector('.profile__name');
 const profileAbout = profileElement.querySelector('.profile__about');
-const popupName = document.querySelector('#name');
-const popupAbout = document.querySelector('#about');
+const profileFormName = document.querySelector('#name');
+const profileFormAbout = document.querySelector('#about');
 
-function popupOpen() {
-    popupElement.classList.add('popup_opened');
-    popupName.value = profileName.textContent;
-    popupAbout.value = profileAbout.textContent;
-}
-popupOpenButtonElement.addEventListener('click', popupOpen);
+const zoomPopup = document.querySelector('#zoom-popup');
+const zoomImage = document.querySelector('.popup__img');
+const zoomImageTitle = document.querySelector('.popup__img-title');
 
-function popupClose() {
-    popupElement.classList.remove('popup_opened');
+//popup opening
+
+function popupOpen(elem) {
+    elem.classList.add('popup_opened');
 }
-popupCloseButtonElement.addEventListener('click', popupClose);
 
 function profileInsert(evt) {
-    evt.preventDefault();
-    profileName.textContent = popupName.value;
-    profileAbout.textContent = popupAbout.value;
-    popupClose();
+  evt.preventDefault();
+  profileName.textContent = profileFormName.value;
+  profileAbout.textContent = profileFormAbout.value;
+  popupClose(profilePopup);
 }
-popupFormElement.addEventListener('submit', profileInsert);
+
+//popup closing
+
+function popupClose(elem) {
+    elem.classList.remove('popup_opened');
+}
+
+profileEdit.addEventListener('click', function() {
+    popupOpen(profilePopup);
+});
+
+buttonClose.forEach( element => {
+    element.addEventListener('click', function() {
+        popupClose(profilePopup);
+        popupClose(createPopup);
+        popupClose(zoomPopup);
+})});
+
+
+profileEdit.addEventListener('click', function() {
+    popupOpen(profilePopup);
+    profileFormName.value = profileName.textContent;
+    profileFormAbout.value = profileAbout.textContent;
+});
+
+profileForm.addEventListener('submit', profileInsert);
+
+buttonAdd.addEventListener('click', function() {
+    popupOpen(createPopup);
+    createFormTitle.value = 'Название';
+    createFormLink.value = 'Ссылка'; 
+});
+
+//aad images
+
+const initialCards = [
+    {
+      name: 'Эльбрус',
+      link: './images/elbrus.jpg'
+    },
+    {
+      name: 'Водопады Агуры',
+      link: './images/waterfall.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: './images/chelyaba.jpg'
+    },
+    {
+      name: 'Увильды',
+      link: './images/uvildy.jpg'
+    },
+    {
+      name: 'Зюраткуль',
+      link: './images/zuratkul.jpg'
+    },
+    {
+      name: 'Иремель',
+      link: './images/iremel.jpg'
+    }
+  ];
+
+initialCards.forEach(function (element) {
+  const cardsTemplate = document.querySelector('#card-template').content;
+  const cardsContainer = document.querySelector('.elements__table');
+  const cardElement = cardsTemplate.querySelector('.element').cloneNode(true);
+  const cardImage = cardElement.querySelector('.element__img');
+  cardImage.src = element.link;
+  cardElement.alt = element.name;
+  cardElement.querySelector('.element__text').textContent = element.name;
+  
+  const likeButton = cardElement.querySelector('.like');
+  likeButton.addEventListener('click', () => {
+    likeButton.classList.toggle('like_active');
+  });
+
+  const deleteButton = cardElement.querySelector('.delete-button');
+  deleteButton.addEventListener('click', function() {
+    cardElement.remove();
+  });
+
+  cardImage.addEventListener('click', () => openPopupZoomImage(cardElement, cardImage));
+  
+  cardsContainer.append(cardElement);
+});
+
+//creating card
+
+function createCard(evt) {
+  evt.preventDefault();
+  const cardsTemplate = document.querySelector('#card-template').content;
+  const cardsContainer = document.querySelector('.elements__table');
+  const cardElement = cardsTemplate.querySelector('.element').cloneNode(true);
+  const cardImage = cardElement.querySelector('.element__img');
+  const createFormTitle = document.querySelector('#title');
+  const createFormLink = document.querySelector('#link');
+  cardImage.src = createFormLink.value;
+  cardElement.alt = createFormTitle.value;
+  cardElement.querySelector('.element__text').textContent = createFormTitle.value;
+  
+  const likeButton = cardElement.querySelector('.like');
+  likeButton.addEventListener('click', () => {
+    likeButton.classList.toggle('like_active');
+  });
+
+  const deleteButton = cardElement.querySelector('.delete-button');
+    deleteButton.addEventListener('click', function() {
+    cardElement.remove();
+  });
+
+  cardImage.addEventListener('click', () => openPopupZoomImage(cardElement, cardImage));
+
+  cardsContainer.prepend(cardElement);
+  popupClose(createPopup);
+}
+
+createForm.addEventListener('submit',createCard);
+
+function openPopupZoomImage(element, image) {
+  zoomImage.src = image.getAttribute('src'); 
+  zoomImage.alt = image.getAttribute('alt');
+  zoomImageTitle.textContent = element.querySelector('.element__text').textContent;
+  popupOpen(zoomPopup);
+};
